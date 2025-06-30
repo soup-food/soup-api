1045a6bdce09291477327aa3b505db4a9e834869
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2025-06-30 16:02:07 +0200

    fix .gitingore

--- backend/src/sus_food/main.py
+++ backend/src/sus_food/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
-
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 
 
-@app.get("/")
+client = AsyncIOMotorClient(MONGODB_CONNECTION_STRING, uuidRepresentation="standard")
+db = client.todolist
+todos = db.todos
+
+@app.get("/hello")
 def read_root():
     return {"message": "Hello Madda!!"}
+
+@app.get("/likes")
+def read_likes():
+    return
